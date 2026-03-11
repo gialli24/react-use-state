72d1d192423a18d0f3e6a7c516ef31fca12e07d7
gialli24 <a.gialli2409@gmail.com>
2026-03-11 12:43:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+
+function App() {
+
+  return (
+    <>
+      <AppHeader />
+      <AppMain />
+    </>
+  )
+}
+
+export default App
