2ec58070b99d051d87ec8889fbfe1db9dcef49e3
gialli24 <a.gialli2409@gmail.com>
2026-03-11 14:56:17 +0100

    add main layout with tabs and button styles

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,24 @@
 export default function AppMain() {
     return (
         <>
+            <main>
+                <section className="tabs">
+                    <div className="container">
+                        <nav>
+                            <button className="btn active">HTML</button>
+                            <button className="btn primary">CSS</button>
+                            <button className="btn primary">JavaScript</button>
+                            <button className="btn primary">Node.js</button>
+                            <button className="btn primary">Express</button>
+                            <button className="btn primary">ReactJS</button>
+                        </nav>
 
+                        <div className="tab-content">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, explicabo possimus. Rem laudantium minus optio facere omnis ea, cum iusto impedit fugiat placeat dolor quaerat nobis a, velit sequi soluta?
+                        </div>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
