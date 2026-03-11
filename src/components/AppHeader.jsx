7a9c5dd80307f13e93c504aeaede8a027a944e5b
gialli24 <a.gialli2409@gmail.com>
2026-03-11 12:47:31 +0100

    add initial CSS reset and header component structure

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,11 @@
 export default function AppHeader() {
     return (
         <>
+            <header>
+                <div className="container">
+                    <h1>Learn Web development</h1>
+                </div>
+            </header>
         </>
     )
 }
